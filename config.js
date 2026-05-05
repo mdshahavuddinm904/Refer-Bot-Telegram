e17a87341d44a2468a8ae1b6bafda39068cc9612
mdshahavuddinm904 <mdshahavuddinm@gmail.com>
2026-05-05 17:43:42 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "8663292414:AAE-8w0OwurrPENy3ntuZtfx9BLy37hu-6s",
-  ADMIN_ID: 8136997138, // 👉 এখানে নিজের Telegram ID দে
-  CHANNELS: ["@Global_Method_Channel"]
+  BOT_TOKEN: "YOUR_BOT_TOKEN",
+  ADMIN_ID: 8136997138,
+  CHANNEL: "@Global_Method_Channel"
 };
